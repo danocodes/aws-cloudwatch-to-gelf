8254f61fd208c13d96f4fe9d89fbb33b273e2026
Dan Torrey <dan.torrey@reserveinteractive.com>
2018-08-05 15:26:36 -0500

    Add properties files for configuration. Add PropertyReader to read properties (with test).

---
+++ src/main/java/PropertyReader.java
@@ -0,0 +1,29 @@
+import java.io.IOException;
+import java.util.Properties;
+
+public class PropertyReader {
+
+    private static String[] PROPERTIES_FILES = { "aws.properties", "graylog.properties" };
+
+    public static String getProperty( String propertyKey ) {
+
+        try {
+            for ( String fileName : PROPERTIES_FILES ) {
+
+                Properties propertiesFile = new Properties();
+                propertiesFile.load( PropertyReader.class.getClassLoader().getResourceAsStream( fileName ) );
+
+                String propertyValue = propertiesFile.getProperty( propertyKey );
+                if ( propertyValue != null ) {
+                    return propertyValue;
+                }
+            }
+        }
+        catch ( IOException ex ) {
+            // Absorb the exception for now.
+            ex.printStackTrace();
+        }
+
+        return null;
+    }
+}
---
+++ src/test/java/PropertyReaderTest.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class PropertyReaderTest {
+
+    @Test
+    public void testReadProperties() throws Exception {
+
+        // Just make sure we can read at least one property.
+        String graylogHost = PropertyReader.getProperty( "graylogHost" );
+        assertEquals( "http://graylog.example.org:12202/gelf", graylogHost );
+    }
+}
